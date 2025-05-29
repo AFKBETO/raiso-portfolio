8cdc479b54ba757a9b0dc088b24b1ff9826d9e5d
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-05-29 16:45:40 +0200

    feat: setup tailwind and other nuxt modules

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,27 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
 	collections: {
 		content: defineCollection({
 			type: 'page',
-			source: '**/*.md'
-		})
+			source: '**/**.md',
+			schema: z.object({
+				title: z.string(),
+				description: z.string().optional(),
+				language: z.string(),
+				date: z.string().optional(),
+			}),
+		}),
+		navbar: defineCollection({
+			type: 'data',
+			source: 'navbar/**.yml',
+			schema: z.object({
+				language: z.string(),
+				home: z.string(),
+				about: z.string(),
+				activities: z.string(),
+				contact: z.string(),
+			})
+		}),
 	}
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,38 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/ui'
-  ]
-})
+	compatibilityDate: '2025-05-15',
+	devtools: { enabled: true },
+	debug: true,
+	colorMode: {
+		preference: 'light', // default value of $colorMode.preference
+		fallback: 'light', // fallback value if not system preference found
+		storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+		storageKey: 'nuxt-color-mode'
+	},
+	modules: [
+		'@nuxt/content',
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxt/image',
+		'@nuxt/scripts',
+		'@nuxt/ui'
+	],
+	css: ['~/assets/css/main.css'],
+	vite: {
+		plugins: [
+			tailwindcss()
+		]
+	},
+	content: {
+		build: {
+			markdown: {
+				highlight: {
+					theme: 'github-light',
+				}
+			}
+		}
+	}
+});
