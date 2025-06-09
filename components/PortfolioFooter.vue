93c0b334e5afcd6098440716ba85796aa8fd2f04
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-06-09 10:59:22 +0200

    fix: fix scrollToTop button and footer position

--- components/PortfolioFooter.vue
+++ components/PortfolioFooter.vue
@@ -1,53 +1,48 @@
 <script setup lang="ts">
-	const locale = 'fr';
+const locale = 'fr';
 
-	const { data: footerLabels } = await useAsyncData('/footer', () => {
-		return queryCollection('footer')
-			.where('language', '=', locale)
-			.first();
-	});
+const { data: footerLabels } = await useAsyncData('/footer', () => {
+	return queryCollection('footer')
+		.where('language', '=', locale)
+		.first();
+});
 
-	const visible = "!fixed bottom-5 end-5 rounded-full";
-  const invisible = visible + " invisible";
-  const classNames = ref(invisible);
+const visible = "!fixed bottom-10 end-5 rounded-full";
+const invisible = visible + " invisible";
+const classNames = ref(invisible);
 
-  function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: "smooth" });
+}
 
-  function handleScroll() {
-    if (window.scrollY > 0) {
-      classNames.value = visible;
-    } else {
-      classNames.value = invisible;
-    }
-  };
+function handleScroll() {
+	if (window.scrollY > 0) {
+		classNames.value = visible;
+	} else {
+		classNames.value = invisible;
+	}
+};
 
-  onMounted(() => {
-    window.addEventListener("scroll", handleScroll);
-  });
+onMounted(() => {
+	window.addEventListener("scroll", handleScroll);
+});
 
-  onUnmounted(() => {
-    window.removeEventListener("scroll", handleScroll);
-  });
+onUnmounted(() => {
+	window.removeEventListener("scroll", handleScroll);
+});
 </script>
 
 <template>
-  <footer class="p-8 bg-white dark:bg-neutral-900 w-full justify-between flex flex-row mt-auto">
-    <p>
-      {{ footerLabels?.body }}
-    </p>
-    <p>
-      Powered by <ULink to="https://github.com/AFKBETO" target="_blank" external>AFKBETO</ULink>
-    </p>
-    <UTooltip text="Scroll to top">
-      <UButton
-        size="xl"
-        color="neutral"
-        :class="classNames"
-        icon="i-lucide-triangle"
-        variant="outline"
-        @click="scrollToTop"/>
-    </UTooltip>
-  </footer>
+	<footer class="p-6 bg-white dark:bg-neutral-900 w-full justify-between flex flex-row mt-auto">
+		<p>
+			{{ footerLabels?.body }}
+		</p>
+		<p>
+			Powered by <ULink to="https://github.com/AFKBETO" target="_blank" external>AFKBETO</ULink>
+		</p>
+		<UTooltip text="Scroll to top">
+			<UButton size="xl" color="neutral" :class="classNames" icon="i-lucide-triangle" variant="outline"
+				@click="scrollToTop" />
+		</UTooltip>
+	</footer>
 </template>
