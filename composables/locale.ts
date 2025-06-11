5ebd2ab1a5977355bc4efa6bd0e7aa5ce6de14e5
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-06-11 20:38:10 +0200

    refactor: simplify locale management by using cookies instead of state

--- composables/locale.ts
+++ composables/locale.ts
@@ -1,11 +1,6 @@
-export const useNuxtLocale = () => useState<string>('locale', () => useDefaultLocale().value);
-export const useDefaultLocale = (fallback = 'fr') => {
-  const locale = ref(fallback);
-  if (import.meta.client) {
-    const navLang = navigator.language;
-    if (navLang) {
-      locale.value = navLang;
-    }
-  }
+export const useNuxtLocale = () => {
+  const locale = useCookie('locale', {
+    default: () => 'fr',
+  });
   return locale;
 };
