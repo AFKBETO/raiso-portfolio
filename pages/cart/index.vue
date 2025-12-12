8eecd8bebcf7afb849e41046942ab5a1866da3da
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-12-12 18:51:51 +0100

    feat(cart): add cart page placeholder with authentication middleware

---
+++ pages/cart/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  colorMode: 'light',
+  middleware: [
+    'auth',
+  ],
+});
+</script>
+
+<template>
+  <div>
+    Your cart here
+  </div>
+</template>
